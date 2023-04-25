6bc39209d0361fb75890a808975261edd988e11d
Juan Ferrer Toribio <juan@verdnatura.es>
2023-04-25 18:42:21 +0200

    refs #5541 Allow to set slave server id

--- mylogger.js
+++ mylogger.js
@@ -119,7 +119,8 @@ module.exports = class MyLogger {
         'updaterows',
         'deleterows'
       ],
-      includeSchema
+      includeSchema,
+      serverId: conf.serverId
     };
 
     if (conf.testMode)
